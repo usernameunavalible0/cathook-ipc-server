97b17f40a55cab281a6d5624eb4b30454432f334
nullifiedcat <nullifiedcat@gmail.com>
2017-08-14 23:59:18 +0300

    arrays start at 0

--- src/server.cpp
+++ src/server.cpp
@@ -78,7 +78,7 @@ void print_status() {
 	ypos += 3;
 	TEXT_NORMAL;
 	// Zeroth peer is the server.
-	for (unsigned i = 1; i < cat_ipc::max_peers; i++) {
+	for (unsigned i = 0; i < cat_ipc::max_peers; i++) {
 		if (!peer().memory->peer_data[i].free) {
 			ESC_CUP(2, ypos);
 			const auto& data = peer().memory->peer_user_data[i];
